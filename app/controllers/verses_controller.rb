19b4d57af570b0ad4fee918334102919fec600eb
cw2981 <cw29810@email.vccs.edu>
2020-03-21 16:24:56 +0000

    added params for verse creation.

--- app/controllers/verses_controller.rb
+++ app/controllers/verses_controller.rb
@@ -6,4 +6,15 @@ class VersesController < ApplicationController
 	def new
 		@verse = Verse.new
 	end
+
+	def create
+		Verse.create(verse_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def verse_params
+		params.require(:verse).permit(:saying, :book, :chapter)
+	end
 end
