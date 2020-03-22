df3fbeb21bb7b42657fd72ecb6b5a469022737f9
cw2981 <cw29810@email.vccs.edu>
2020-03-22 04:48:15 +0000

    added first verse to heroku, going to see if this has resolved recent error.

--- app/models/verse.rb
+++ app/models/verse.rb
@@ -1,5 +1,5 @@
 class Verse < ApplicationRecord
 	validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
 	validates :book, presence: true, length: { maximum: 40, minimum: 5 }
-	validates :chapter, presence: true, length: { maximum: 10, minimum: 0 }
+	validates :chapter, presence: true, length: { maximum: 10 }
 end
